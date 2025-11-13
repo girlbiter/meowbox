f7750c3dfa5a0af12b8dbc5bf571683dd58a9f78
girlbiter <thecatofstardust@gmail.com>
2025-11-13 12:39:19 -0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-sudo pacman -S code wofi waybar nautilus kitty dconf dconf-editor hyprsunset swaync noto-fonts-emoji otf-font-awesome woff2-font-awesome ttf-jetbrains-mono ttf-noto-nerd && \
+sudo pacman -S code wofi waybar nautilus kitty dconf dconf-editor sunsetr swaync noto-fonts-emoji otf-font-awesome woff2-font-awesome ttf-jetbrains-mono ttf-noto-nerd && \
 cp -r MapleMono-TTF /usr/share/fonts/MapleMono-TTF && \
 fc-cache -f -v && \
 cp -r wallpapers ~/Pictures/wallpapers
@@ -8,6 +8,5 @@ sudo pacman -S --needed git base-devel && \
 git clone https://aur.archlinux.org/yay.git && \
 cd yay && \
 makepkg -si && \
-yay -S rose-pine-hyprcursor && \
 gsettings set org.gnome.desktop.interface BreezeX-RosePine-Linux && \
 gsettings set org.gnome.desktop.interface gtk-theme "diinki-retro-dark"
